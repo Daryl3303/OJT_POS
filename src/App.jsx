4d9c5baa5d6ac4598658cb92f636b79d81866281
John Daryl Abkilan <abkilanjohndaryl03@gmail.com>
2025-03-18 10:46:11 +0800

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import POSDashboard from './components/reports'
+
+function App() {
+  
+
+  return (
+   
+      <div className="h-[100vh] w-full">
+        <POSDashboard />
+      </div>
+     
+    
+  )
+}
+
+export default App
